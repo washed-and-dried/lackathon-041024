3b864edbfbfff46cfe2ba792867ce18b63018f4b
arshpsps <arshh.gill@outlook.com>
2024-10-04 14:01:12 +0530

    add.tsx

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,9 +1,22 @@
-import TreeData from "./TreeData.tsx";
 import Mamap from "./Mamap.tsx";
+import "./add.css";
 
 export default function Add() {
     return <>
-        <TreeData/>
-        <Mamap/>
+        <form action="localhost:4000/tree" method="POST">
+            <div>
+                <label id="numberOfTrees">No of Trees</label>
+                <input name="numberOfTrees" type="number"/>
+            </div>
+            <div>
+                <label id="day">Date</label>
+                <input name="day" type="date"/>
+            </div>
+            <div>
+                <label id="plantedBy">Planted By</label>
+                <input name="plantedBy" type="text"/>
+            </div>
+        </form>
+
     </>
 }
